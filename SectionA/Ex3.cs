a053a4f5854c295e74c749991ad78619cf5c6bdd
vishva514 <vishvalingam.kumaran@psiog.com>
2025-10-01 10:39:34 +0530

    program3 added

---
+++ SectionA/Ex3.cs
@@ -0,0 +1,14 @@
+using System;
+namespace Swap
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n1 = 41;
+            double n2 = 9.01;
+            Console.WriteLine(Convert.ToString("Value " + n1));
+            Console.WriteLine(Convert.ToDouble(n2));
+        }
+    }
+}
