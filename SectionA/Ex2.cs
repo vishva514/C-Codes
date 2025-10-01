8eefcf0bfda1fd1ab7fa116d687fb66283cff206
vishva514 <vishvalingam.kumaran@psiog.com>
2025-10-01 10:28:25 +0530

    Program2 added

---
+++ SectionA/Ex2.cs
@@ -0,0 +1,12 @@
+using System;
+namespace App
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = 15;
+            Console.WriteLine(num);     
+        }
+    }
+}
